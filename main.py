a815555c608cab87a29c182337aaad1bf126e5d4
Fuwuffyi <luca.palazzini3@studio.unibo.it>
2025-03-19 21:39:06 +0100

    Make bot send sticker zip file to user

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 import json
+import shutil
 import logging
 import requests
 from telegram import Update, Bot
@@ -29,14 +30,14 @@ async def download_stickers(update: Update, context: ContextTypes.DEFAULT_TYPE):
     emoji_dict = {}
     for (index, sticker) in enumerate(sticker_set.stickers):
         # Save the emoji related to each sticker
-        emoji_dict[str(index)] = sticker.emoji
-        print(sticker.emoji)
+        index_str = f"{index:03d}"
+        emoji_dict[index_str] = sticker.emoji
         # Save stickers to file if not exists
-        if os.path.exists(f"downloads/{set_name}/{index}.webp"):
+        if os.path.exists(f"downloads/{set_name}/{index_str}.webp"):
             continue
         sticker_url = (await Bot.get_file(context.bot, sticker)).file_path
         sticker_response = requests.get(sticker_url)
-        with open(f"downloads/{set_name}/{index}.webp", "wb") as f:
+        with open(f"downloads/{set_name}/{index_str}.webp", "wb") as f:
             f.write(sticker_response.content)
     # Save the emojis to a json file
     with open(f"downloads/{set_name}/emoji.txt", "w", encoding="utf-8") as f:
@@ -46,7 +47,12 @@ async def download_stickers(update: Update, context: ContextTypes.DEFAULT_TYPE):
     with open(f"downloads/{set_name}/title.txt", "w") as f:
         # Write the json file with indentation
         f.write(set_name)
+    # Create a zip archive
     await context.bot.send_message(chat_id=update.effective_chat.id, text=f"Creating zip file for {set_name}...")
+    archived = shutil.make_archive(f'downloads/{set_name}', 'zip', f'downloads/{set_name}/')
+    # Send the zip archive to the user
+    with open(archived, 'rb') as zip_file:
+        await Bot.sendDocument(context.bot, chat_id=update.effective_chat.id, document=zip_file)
 
 if __name__ == '__main__':
     # Create the bot
